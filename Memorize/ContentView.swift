fb643e5c23a651a1db3be7a5589012a0b2a0de13
André Almeida <andre_rdo@hotmail.com>
2020-11-06 18:47:48 -0300

    First commit

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -7,12 +7,46 @@
 
 import SwiftUI
 
+
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
+        return ZStack(content: {
+            RoundedRectangle(cornerRadius: 10.0).stroke()
+            Text("👻")
+        })
             .padding()
+            .foregroundColor(Color.orange)
+        
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
